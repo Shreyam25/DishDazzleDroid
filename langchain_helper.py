2db0763c0f2f5adecacc69dd71736d7221630244
Shreya Mainkar <89998182+Shreyam25@users.noreply.github.com>
2024-03-27 22:48:53 +0530

    Add files via upload

--- langchain_helper.py
+++ langchain_helper.py
@@ -13,8 +13,8 @@ llm= GooglePalm(temperature=0.23,google_api_key=api)
 
 def get_recipe_ideas(ingredients):
     prompt = (
-        "You are a helpful assistant who provides recipes given available ingredients. Your task is to suggest 3 delicious recipe ideas using only the listed ingredients. Your recommendations should be presented in the following format:\n"
-        "<h4 style='font-size: larger; font-weight: bold; text-decoration: underline;'>Recipe: {title}</h4>\n"
+        "<SYS> You are a helpful assistant who is expert at providing recipes given available ingredients only.You do not adds unavailable ingrediants.Your task is to generate three recipe ideas using only the listed ingredients.The input will consist of a list of available ingredients provided by the user.</SYS>. Ensure that the suggested recipes do not include any ingredients other than those mentioned by the user."
+         "The format for presenting the recipe ideas should include the name of the dish and a brief description of the recipe. Your recommendations should be presented in the following format:\n"        "<h4 style='font-size: larger; font-weight: bold; text-decoration: underline;'>Recipe: {title}</h4>\n"
         "<h5 style='font-weight: bold; '>Ingredients:</h5> {ingredients}\n"
         "<h5 style='font-weight: bold; '>Instructions:</h5> {instructions}\n"
         "\n\n\n"
